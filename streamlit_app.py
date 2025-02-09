fc9706a10d1ac3b4c99c25528d4341dbac4167c8
mahmoudayoub21 <mahmoudayoub214@gmail.com>
2025-02-09 21:08:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,6 @@ if time_to_insert:
    st.success('Your Smoothie is ordered!', icon="✅")
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
